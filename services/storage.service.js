8dd17f9dd4bf435f67bfd9511829a4c6910e3052
Marina <mp@friendwork.ru>
2022-01-20 09:48:56 +0300

    OS Path

---
+++ services/storage.service.js
@@ -0,0 +1,10 @@
+import {homedir} from 'os';
+import {join, dirname, extname} from 'path'
+
+const filePath = join(homedir(), 'wether-data.json');
+
+const saveKeyValue = (key, value) => {
+
+}
+
+export {saveKeyValue}
--- weather.js
+++ weather.js
@@ -1,17 +1,19 @@
 #!//usr/bin/env node
 import {getArgs} from './helpers/args.js';
+import {printHelp} from './services/log.service.js';
+import {saveKeyValue} from './services/storage.service.js';
 
 const initCLI = () => {
   const args = getArgs(process.argv);
-  console.log(args);
+
   if (args.h) {
-    // help information
+    printHelp()
   }
   if (args.s) {
     // save city settings
   }
   if (args.t) {
-    // save token
+    saveKeyValue('token', args.t)
   }
   // show weather
 }
